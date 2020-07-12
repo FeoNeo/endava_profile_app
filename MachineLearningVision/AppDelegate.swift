a5da826a042af0da0bde0a536c23bda94b88bef8
FeoNeo <112518958+FeoNeo@users.noreply.github.com>
2020-07-12 14:53:04 +0000

    ditto

--- MachineLearningVision/AppDelegate.swift
+++ MachineLearningVision/AppDelegate.swift
@@ -2,4 +2,5 @@
 //  AppDelegate.swift
 //  MachineLearningVision
 //
-//  Created by Kelly Galakatos on 1
+//  Created by Kelly Galakatos on 11/30/18.
+//  Copyright © 2018 Kelly Galakatos. All rights rese
