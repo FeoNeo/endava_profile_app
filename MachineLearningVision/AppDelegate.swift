2acf4f75e4e1ef49463091844e85998bfdbc9377
FeoNeo <112518958+FeoNeo@users.noreply.github.com>
2020-07-19 08:02:05 +0000

    Update internal deps

--- MachineLearningVision/AppDelegate.swift
+++ MachineLearningVision/AppDelegate.swift
@@ -3,4 +3,10 @@
 //  MachineLearningVision
 //
 //  Created by Kelly Galakatos on 11/30/18.
-//  Copyright © 2018 Kelly Galakatos. All rights rese
+//  Copyright © 2018 Kelly Galakatos. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class 
