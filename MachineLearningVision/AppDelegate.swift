28a85db765fa76042b88e464b9d8ffd2c35acc84
FeoNeo <112518958+FeoNeo@users.noreply.github.com>
2020-08-23 17:02:27 +0000

    renames namespace

--- MachineLearningVision/AppDelegate.swift
+++ MachineLearningVision/AppDelegate.swift
@@ -24,4 +24,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
-    func applicationDidEnter
+    func applicationDidEnterBackground(_ application: UIApplication) {
+   
