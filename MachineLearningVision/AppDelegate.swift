8231be3d0928f069028cc963782f65b39b0c742d
FeoNeo <112518958+FeoNeo@users.noreply.github.com>
2020-08-23 12:32:43 +0000

    Updated service API models for release.

--- MachineLearningVision/AppDelegate.swift
+++ MachineLearningVision/AppDelegate.swift
@@ -21,4 +21,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this metho
+        // Use this method to pause ongoing tasks, disable timers, and invalidate gr
