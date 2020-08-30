bc09d1caa1c4b3d218b3a397b78e2d7e4c19419d
FeoNeo <112518958+FeoNeo@users.noreply.github.com>
2020-08-30 10:27:19 +0000

    sc1200_wdt: Fix oops

--- MachineLearningVision/AppDelegate.swift
+++ MachineLearningVision/AppDelegate.swift
@@ -25,4 +25,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application s
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state
