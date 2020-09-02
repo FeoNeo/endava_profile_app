f5175a67a1f6305ce2696f06511e3e919962dd66
FeoNeo <112518958+FeoNeo@users.noreply.github.com>
2020-09-02 15:40:53 +0000

    generics

--- MachineLearningVision/AppDelegate.swift
+++ MachineLearningVision/AppDelegate.swift
@@ -30,4 +30,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of th
+        // Called as part of the transition from the background to the active state; h
