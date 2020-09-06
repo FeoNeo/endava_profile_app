3aaf5bdd0a191dc51dcf18dc60f959984715f820
FeoNeo <112518958+FeoNeo@users.noreply.github.com>
2020-09-06 10:30:20 +0000

    100

--- MachineLearningVision/AppDelegate.swift
+++ MachineLearningVision/AppDelegate.swift
@@ -33,4 +33,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidB
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Re
