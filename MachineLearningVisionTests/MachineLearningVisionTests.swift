6171d429f5a263b90bc7ef66855ae43c4f786074
FeoNeo <112518958+FeoNeo@users.noreply.github.com>
2023-03-12 05:54:52 +0000

    Update normalization.md

--- MachineLearningVisionTests/MachineLearningVisionTests.swift
+++ MachineLearningVisionTests/MachineLearningVisionTests.swift
@@ -13,3 +13,7 @@ class MachineLearningVisionTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown co
