a2a8529911eb7f3e4be0e2bb1204456dd440f354
FeoNeo <112518958+FeoNeo@users.noreply.github.com>
2023-03-16 06:50:24 +0000

    modified css

--- MachineLearningVisionTests/MachineLearningVisionTests.swift
+++ MachineLearningVisionTests/MachineLearningVisionTests.swift
@@ -27,4 +27,7 @@ class MachineLearningVisionTests: XCTestCase {
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
-            // Put the code
+            // Put the code you want to measure the time of here.
+        }
+    }
+
