6ffc646ae7385b4efa3ce9a0e6b68de448d8a0dc
FeoNeo <112518958+FeoNeo@users.noreply.github.com>
2023-03-19 19:03:38 +0000

    fix jenkins

--- MachineLearningVisionUITests/MachineLearningVisionUITests.swift
+++ MachineLearningVisionUITests/MachineLearningVisionUITests.swift
@@ -13,4 +13,5 @@ class MachineLearningVisionUITests: XCTestCase {
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
-        // In UI tests it is us
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+     
