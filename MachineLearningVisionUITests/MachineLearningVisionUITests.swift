0bf94a37405d03183746e1ab8c55d95fc39eaee1
FeoNeo <112518958+FeoNeo@users.noreply.github.com>
2023-03-19 14:45:37 +0000

    moar testing

--- MachineLearningVisionUITests/MachineLearningVisionUITests.swift
+++ MachineLearningVisionUITests/MachineLearningVisionUITests.swift
@@ -11,4 +11,6 @@ import XCTest
 class MachineLearningVisionUITests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is us
