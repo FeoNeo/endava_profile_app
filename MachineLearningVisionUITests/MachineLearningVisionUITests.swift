afa08fb5d471f3e3f02ed13d22e3d6169eeeeb90
FeoNeo <112518958+FeoNeo@users.noreply.github.com>
2023-03-20 11:02:43 +0000

    Suppress warning

--- MachineLearningVisionUITests/MachineLearningVisionUITests.swift
+++ MachineLearningVisionUITests/MachineLearningVisionUITests.swift
@@ -16,4 +16,4 @@ class MachineLearningVisionUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // UI tests must 
+        // UI tests must launch the application that they test. Doing 
