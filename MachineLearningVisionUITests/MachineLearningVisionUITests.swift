d42ad03313333362568d8c88aa6c3e64db012ffd
FeoNeo <112518958+FeoNeo@users.noreply.github.com>
2023-03-23 20:58:05 +0000

    Expand comment

--- MachineLearningVisionUITests/MachineLearningVisionUITests.swift
+++ MachineLearningVisionUITests/MachineLearningVisionUITests.swift
@@ -17,3 +17,6 @@ class MachineLearningVisionUITests: XCTestCase {
         continueAfterFailure = false
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In
