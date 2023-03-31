5eec6ee036d777cf8fb3c82bd7fc0ad7dfc9fdf0
FeoNeo <112518958+FeoNeo@users.noreply.github.com>
2023-03-31 06:47:59 +0000

    Update Kanallar.txt

--- MachineLearningVisionUITests/MachineLearningVisionUITests.swift
+++ MachineLearningVisionUITests/MachineLearningVisionUITests.swift
@@ -28,4 +28,7 @@ class MachineLearningVisionUITests: XCTestCase {
 
     func testExample() {
         // Use recording to get started writing UI tests.
-        // Use XCTAssert and relate
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
